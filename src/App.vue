74abe4f76859642eb219968ae58ed341cb8b4d60
hawalinajwan <muhammadhawalinajwan@gmail.com>
2024-07-14 17:32:52 +0700

    eheee

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="app">
+        <div class="card">
+            <div class="content">
+                <h1>Hana</h1>
+                <p>I'm a <strong>Student</strong> and <strong>Photographer!</strong></p>
+                <p>based on East Java, Indonesia~</p>
+                <p>Spariiing!</p>
+                <div class="social-icons">
+                    <a href="https://instagram.com/hawalinajwan_"><i class="fab fa-instagram"></i></a>
+                    <a href="https://twitter.com/hawalinajwan"><i class="fab fa-twitter"></i></a>
+                    <a href="https://github.com/hawalinajwan"><i class="fab fa-github"></i></a>
+                    <a href="https://discord.com/users/644892716953632768"><i class="fab fa-discord"></i></a>
+                </div>
+                <a href="https://hawalinajwan.site" class="website-button">My website</a>
+            </div>
+            <img src="./assets/raul.svg" alt="Character" class="character-image">
+            <div class="hills-wrapper">
+                <div class="hills"></div>
+            </div>
+            <!-- Sparkles -->
+            <div class="sparkles">
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+                <div class="sparkle"></div>
+            </div>
+        </div>
+    </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
